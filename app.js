65790db368d731929e6eef5eda76c9ce37e84e69
Vivek <72148636+mahi-19@users.noreply.github.com>
2021-09-09 01:13:17 +0530

    Bug Fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const microphoneButton = document.getElementById("microphoneButton");
 
 microphoneButton.addEventListener("click", function () {
 	window.open(
-		"http://localhost:5500/mic/",
+		"http://127.0.0.1:5501/mic/",
 		"nativeWindow",
 		"width=350,height=300"
 	);
